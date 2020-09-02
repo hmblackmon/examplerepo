9e7f63433a24aead352d82b429ffbb3a727c5193
Hunter Blackmon <HunterBlackmon@Ks-MacBook-Pro.local>
2020-09-02 14:14:13 -0500

    another change to the message

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace examplerepo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Git is great..... It really is");
+            Console.WriteLine("Git is great!");
         }
     }
 }
